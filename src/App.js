9f93f01141a73ca13a3b6a4998e960ce0f2454cd
PraisesPJMT <praisesmusa@outlook.com>
2022-09-15 18:05:59 +0100

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <>
-    </>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+  </Router>
+);
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+  let navLinks = [
+    {
+      id: 1,
+      name: 'BOOKS',
+      ref: '/books',
+    },
+    {
+      id: 2,
+      name: 'CATEGORIES',
+      ref: '/categories',
+    },
+  ];
+  navLinks = navLinks.map((link) => (
+    <NavLink
+      className={link.name.toLowerCase()}
+      to={link.ref}
+      key={link.id}
+    >
+      { link.name.toUpperCase() }
+    </NavLink>
+  ));
+  return (
+    <header className="header">
+      <Link
+        className="logo"
+        to="/"
+      >
+        <h2>
+          Bookstore CMS
+        </h2>
+      </Link>
+      <nav className="nav">
+        { navLinks }
+      </nav>
+      <span className="profile">
+        <FontAwesomeIcon
+          className="profile-icon"
+          icon={faUser}
+        />
+      </span>
+    </header>
+  );
+};
+
+export default Header;
