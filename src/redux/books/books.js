8f0e2a3e1e811903f9492be003d7664441b6a615
PraisesPJMT <praisesmusa@outlook.com>
2022-09-20 17:28:50 +0100

    Add books to initial state of book reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,34 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookstore-rx/bookReducer/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore-rx/bookReducer/REMOVE_BOOK';
 
-const initialBooksState = [];
+const initialBooksState = [
+  {
+    id: uuidv4(),
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    tag: 'Action',
+    progress: 64,
+    chapter: 'Chapter 17',
+  },
+  {
+    id: uuidv4(),
+    title: 'Dune',
+    author: 'Frank Herbert',
+    tag: 'Science Fiction',
+    progress: 8,
+    chapter: 'Chapter 3: "A Lesson Learned"',
+  },
+  {
+    id: uuidv4(),
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    tag: 'Economy',
+    progress: 0,
+    chapter: 'Introduction',
+  },
+];
 
 const addBook = ({ id, title, author }) => (
   {
