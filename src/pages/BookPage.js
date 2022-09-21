4a32912c1e83bd71837adb8ed45ce2f3c0d51dbb
PraisesPJMT <praisesmusa@outlook.com>
2022-09-21 16:49:55 +0100

    Implement fetching books from API

--- src/pages/BookPage.js
+++ src/pages/BookPage.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from '../components/Book';
 import AddBook from '../components/AddBook';
+import { fetchBooks } from '../redux/books/books';
 
 const BookPage = () => {
   const bookCatalog = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, []);
   const books = bookCatalog.map((book) => (
     <Book
       key={book.id}
       id={book.id}
       title={book.title}
       author={book.author}
-      tag={book.tag}
-      progress={book.progress}
-      chapter={book.chapter}
+      tag={book.category}
     />
   ));
 
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,33 +1,37 @@
-import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
+const apiID = 'NhIhGUO8xPYciDZcFhpZ';
+const baseURL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${apiID}/books`;
 
 const ADD_BOOK = 'bookstore-rx/bookReducer/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore-rx/bookReducer/REMOVE_BOOK';
+const FETCH_BOOKS = 'bookstore-rx/bookReducer/FETCH_BOOKS';
 
 const initialBooksState = [
-  {
-    id: uuidv4(),
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    tag: 'Action',
-    progress: 64,
-    chapter: 'Chapter 17',
-  },
-  {
-    id: uuidv4(),
-    title: 'Dune',
-    author: 'Frank Herbert',
-    tag: 'Science Fiction',
-    progress: 8,
-    chapter: 'Chapter 3: "A Lesson Learned"',
-  },
-  {
-    id: uuidv4(),
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    tag: 'Economy',
-    progress: 0,
-    chapter: 'Introduction',
-  },
+  // {
+  //   id: uuidv4(),
+  //   title: 'The Hunger Games',
+  //   author: 'Suzanne Collins',
+  //   tag: 'Action',
+  //   progress: 64,
+  //   chapter: 'Chapter 17',
+  // },
+  // {
+  //   id: uuidv4(),
+  //   title: 'Dune',
+  //   author: 'Frank Herbert',
+  //   tag: 'Science Fiction',
+  //   progress: 8,
+  //   chapter: 'Chapter 3: "A Lesson Learned"',
+  // },
+  // {
+  //   id: uuidv4(),
+  //   title: 'Capital in the Twenty-First Century',
+  //   author: 'Suzanne Collins',
+  //   tag: 'Economy',
+  //   progress: 0,
+  //   chapter: 'Introduction',
+  // },
 ];
 
 const addBook = ({ id, title, author }) => (
@@ -46,6 +50,19 @@ const removeBook = (id) => (
   }
 );
 
+const fetchBooks = () => async (dispatch) => {
+  const fetchResponse = await axios.get(baseURL);
+  const fetchedData = await fetchResponse.data;
+  const bookCatalog = Object.entries(fetchedData).map((book) => ({
+    id: book[0],
+    ...book[1][0],
+  })) || [];
+  dispatch({
+    type: FETCH_BOOKS,
+    books: bookCatalog,
+  });
+};
+
 const bookReducer = (state = initialBooksState, action) => {
   switch (action.type) {
     case ADD_BOOK:
@@ -61,11 +78,13 @@ const bookReducer = (state = initialBooksState, action) => {
       return [
         ...state.filter((book) => book.id !== action.id),
       ];
+    case FETCH_BOOKS:
+      return action.books;
     default:
       return state;
   }
 };
 
-export { addBook, removeBook };
+export { addBook, removeBook, fetchBooks };
 
 export default bookReducer;
